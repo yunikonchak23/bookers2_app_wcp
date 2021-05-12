1787ed07e472305b9d9d245a65ff9903265cd2da
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-05-12 10:53:51 +0000

    [Add]投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   root to:'homes#top'
+
+  resources :books
 end
