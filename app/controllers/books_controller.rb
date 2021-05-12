d5c35a00b9170ab894ba35207a6ecf5263314edb
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-05-12 18:16:41 +0000

    [Add]scssとbooks.controller

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,8 +3,14 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
-    @book.save
-    redirect_to book_path(@book)
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book)
+    else
+      @user = current_user
+      @books = Book.all
+      render :index
+    end
   end
 
   def index
