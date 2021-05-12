4a44449761519d7f6310235d6d6fa00e1cda80db
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-05-12 11:28:27 +0000

    [Add]User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = where(user_id: @user.id)
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 end
