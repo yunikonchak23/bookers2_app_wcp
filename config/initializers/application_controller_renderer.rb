f584074b3c33e875ac52d7c08cc7364bafd9264d
yunikonchak23 <uetamago.btssuga@gmail.com>
2021-05-13 12:40:22 +0000

    [Add]メニューバーデザイン

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '3f373c4733b1303d4eb437fae482e1a4e8a33090d9b494d908edfd3713c1634f809ae9d47d6655c5e45a0ee130c32cee16838af440488062bb275d58c2605b16'
